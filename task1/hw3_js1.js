96892533afa92145f9cb8218f3594187b83319b1
maxve1 <emvi@tut.by>
2021-11-01 11:45:02 +0300

    1 fixed , 3 fixed

--- task1/hw3_js1.js
+++ task1/hw3_js1.js
@@ -1,23 +1,23 @@
 var fs = require('fs');
 const dat = fs.readFileSync('data.json', 'utf8');
 
-const EurUsd = 0.8622; //API works only in paid mode :(
+const eurUsd = 0.8622; //API works only in paid mode :(
 
 let j = JSON.parse(dat); 
 
 let jEur = {}; 
 jEur = JSON.parse(JSON.stringify(j));
 
- let Z = [];
- let Y = [];
+ let z = [];
+ let y = [];
 
 for(let i=0;i< jEur.data.length;i++){
-     Z[i] = Object.values(jEur.data[i].Prices);   
+     z[i] = Object.values(jEur.data[i].Prices);   
 
-     Y[i] = Z[i].map(k=> k * EurUsd);
-     jEur.data[i].Prices.Price = Number((Y[i][0]).toFixed(2));
-     jEur.data[i].Prices["Retail Price"] = Number((Y[i][1]).toFixed(2));
-     jEur.data[i].Prices["Wholesale Price"] = Number((Y[i][2]).toFixed(2));  
+     y[i] = z[i].map(k=> k * eurUsd);
+     jEur.data[i].Prices.Price = Number((y[i][0]).toFixed(2));
+     jEur.data[i].Prices["Retail Price"] = Number((y[i][1]).toFixed(2));
+     jEur.data[i].Prices["Wholesale Price"] = Number((y[i][2]).toFixed(2));  
      jEur.data[i].Prices.Currency = 'Euro';
      j.data[i].Prices.Currency = 'Dollar';
 }     
--- task3/animals.js
+++ task3/animals.js
@@ -7,6 +7,10 @@ class Animal {
         this.color = color
         this.place_of_origin = place_of_origin                 
     }
+    get getInfo(){
+        return `Name - ${this._name} type - ${this._type}, color - ${this._color}, weight - ${this._weight}, height - ${this._height}, place of origin - ${this._place_of_origin}`
+    }
+
     get name(){
         return this._name
     }
@@ -83,6 +87,9 @@ class Snakes extends Animal{
         super(name, weight, height, type, color, place_of_origin);
         this._isPoisonous = isPoisonous;
     }
+    get getInfo() {
+        return super.getInfo + `, isPoisonous - ${this._isPoisonous}`
+    }
     get isPoisonous(){
         return this._isPoisonous; 
     }
@@ -101,6 +108,10 @@ class Birds extends Animal{
         super(name, weight, height, type, color, place_of_origin);
         this._isFlying = isFlying;
     }
+    get getInfo() {
+        return super.getInfo + `, isFlying - ${this._isFlying}`
+    }
+
     get isFlying(){
         return this._isFlying; 
     }
@@ -119,6 +130,10 @@ class CatLike extends Animal{
         super(name, weight, height, type, color, place_of_origin);
         this._isSafeToPet = isSafeToPet;
     }
+     get getInfo() {
+        return super.getInfo + `, isSafeToPet - ${this._isSafeToPet}`
+    }
+
     get isSafeToPet(){
         return this._isSafeToPet; 
     }
@@ -135,9 +150,11 @@ class CatLike extends Animal{
 
 
 const Cat = new Animal('Barsik',1,1,'Mammal','Black','Europe')
+
 const Gadyuka = new Snakes('Gadyuka',1,1,'Reptile','Black','Asia', true)
 const Eagle = new Birds('Eagle',1,1,'Bird','Black','America', true)
 const Puma = new CatLike('Puma',1,1,'Mammal','Black','America', false)
+console.log(Puma.getInfo);
 console.log(Cat)
 console.log(Gadyuka)
 console.log(Gadyuka.isPoisonous)
